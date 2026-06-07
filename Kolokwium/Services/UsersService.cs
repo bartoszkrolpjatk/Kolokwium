3d89e5a6b20ee8ac542591bcb1169925bd649c4b
bartoszkrolpjatk <s31572@pjwstk.edu.pl>
2026-06-07 11:48:50 +0200

    Update UsersService.cs

--- Kolokwium/Services/UsersService.cs
+++ Kolokwium/Services/UsersService.cs
@@ -1,6 +1,8 @@
-﻿using Kolokwium.Data;
+using Kolokwium.Data;
 using Kolokwium.Dtos;
 using Kolokwium.Exceptions;
+using Kolokwium.Models;
+using Microsoft.EntityFrameworkCore;
 
 namespace Kolokwium.Services;
 
@@ -11,8 +13,29 @@ public class UsersService(DbFirstContext dbContext) : IUsersService
         var transaction =  await dbContext.Database.BeginTransactionAsync();
         try
         {
-            var duplicatedEmail = dbContext.Users.Any(u => u.Email == createUserDto.Email);
-            throw new DuplicatedEmailException($"Email {createUserDto.Email} already exists!");
+            var duplicatedEmail = await dbContext.Users.AnyAsync(u => u.Email == createUserDto.Email);
+            if (duplicatedEmail)
+                throw new DuplicatedEmailException($"Email {createUserDto.Email} already exists!");
+
+            var role = await dbContext.Roles.FirstOrDefaultAsync(r => r.Id == createUserDto.RoleId);
+            if (role == null)
+                throw new NotFoundException($"Role with id {createUserDto.RoleId} does not exist");
+
+            var user = new User()
+            {
+                FirstName = createUserDto.FirstName,
+                LastName = createUserDto.LastName,
+                Email = createUserDto.Email
+            };
+            await dbContext.Users.AddAsync(user);
+
+            var userRole = new UserRole()
+            {
+                AssignedAt = DateTime.Now,
+                Role = role,
+                User = user
+            };
+            await dbContext.UserRoles.AddAsync(userRole);
         }
         catch (Exception e)
         {
