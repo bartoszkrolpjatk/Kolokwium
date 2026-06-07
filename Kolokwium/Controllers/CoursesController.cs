4f9f39784d57a05690e48d12d7743a4b26673705
Bartosz <bartosz.krol@bnpparibas.pl>
2026-06-07 11:12:22 +0200

    db scaffold

---
+++ Kolokwium/Controllers/CoursesController.cs
@@ -0,0 +1,10 @@
+﻿using Microsoft.AspNetCore.Mvc;
+
+namespace Kolokwium.Controllers;
+
+[ApiController]
+[Route("/api/[controller]")]
+public class CoursesController : ControllerBase
+{
+    
+}
--- Kolokwium/Program.cs
+++ Kolokwium/Program.cs
@@ -1,12 +1,30 @@
+using System.Text.Json.Serialization;
+using Kolokwium.Data;
+using Microsoft.EntityFrameworkCore;
+
 var builder = WebApplication.CreateBuilder(args);
 
-// Add services to the container.
-
-builder.Services.AddControllers();
-// Learn more about configuring OpenAPI at https://aka.ms/aspnet/openapi
+builder.Services.AddControllers().AddJsonOptions(options =>
+{
+    options.JsonSerializerOptions.DefaultIgnoreCondition = JsonIgnoreCondition.WhenWritingNull;
+});
 builder.Services.AddOpenApi();
+builder.Services.AddDbContext<DbFirstContext>(opt =>
+{
+    opt.UseSqlServer(builder.Configuration.GetConnectionString("DefaultConnection"));
+});
+builder.Services.AddRouting(options =>
+{
+    options.LowercaseUrls = true;
+});
 
 var app = builder.Build();
+
+if (app.Environment.IsDevelopment())
+{
+    app.MapOpenApi();
+    // app.UseSwaggerUI(options => options.SwaggerEndpoint("/openapi/v1.json", "v1"));
+}
 
 // Configure the HTTP request pipeline.
 if (app.Environment.IsDevelopment())
